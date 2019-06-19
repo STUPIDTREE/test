9e959681ce5f111aa440ca2c4824b382457242e0
STUPIDTREE <975032483@qq.com>
2019-06-19 21:07:03 +0800

    remove_1_remove_test.py

--- 1_remove_test.py
+++
@@ -1,8 +0,0 @@
-def min_count(n):
-    if not isinstance(n, int):#explainationS
-        raise TypeError('bad operand type')
-    m = 1024 - n 
-    num = m//64 + m % 64 //16 + m % 16 // 4 + m %4 
-    return num
-
-print(min_count(200))
